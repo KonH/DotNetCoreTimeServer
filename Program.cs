390cd95721950634c89d10dac7e4a6ca6d895def
KonH <konh@yandex.ru>
2017-01-17 19:49:22 +0700

    First version with correct time format;

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace TimeServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {            
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync(
+                    DateTime.Now.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:sszzz"));
+                                                               
+            });
+        }
+    }
+}
