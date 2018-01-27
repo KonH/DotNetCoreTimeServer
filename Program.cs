ce1dccc8dee0e9271de86fa826683fe611c98af1
KonH <konh@yandex.ru>
2018-01-27 15:43:08 +0700

    Code Formatting;

--- Program.cs
+++ Program.cs
@@ -5,46 +5,38 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 
-namespace TimeServer
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var defaultUrls = new string[]{ "http://*:8080" };
-            var urls = args.Length > 0 ? args : defaultUrls;
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseStartup<Startup>()
-                .UseUrls(urls)
-                .Build();
+namespace TimeServer {
+	public class Program {
+		public static void Main(string[] args) {
+			var defaultUrls = new string[] { "http://*:8080" };
+			var urls = args.Length > 0 ? args : defaultUrls;
+			var host = new WebHostBuilder()
+				.UseKestrel()
+				.UseStartup<Startup>()
+				.UseUrls(urls)
+				.Build();
 
-            host.Run();
-        }
-    }
+			host.Run();
+		}
+	}
 
-    public class Startup
-    {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddRouting();
-        }
+	public class Startup {
+		public void ConfigureServices(IServiceCollection services) {
+			services.AddRouting();
+		}
 
-        public void Configure(IApplicationBuilder app)
-        {  
-            var defaultRouteHandler = new RouteHandler(context =>
-            {
-                return context.Response.WriteAsync("Use GET /time to retrive current UTC time.");
-            });
-            var routeBuilder = new RouteBuilder(app, defaultRouteHandler);
-            routeBuilder.MapRoute("default", "");
-            routeBuilder.MapGet("time", context =>
-            {
-                return context.Response.WriteAsync(
-                    DateTime.Now.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:sszzz"));
-            });
-            var routes = routeBuilder.Build();
-            app.UseRouter(routes);
-        }
-    }
+		public void Configure(IApplicationBuilder app) {
+			var defaultRouteHandler = new RouteHandler(context => {
+				return context.Response.WriteAsync("Use GET /time to retrive current UTC time.");
+			});
+			var routeBuilder = new RouteBuilder(app, defaultRouteHandler);
+			routeBuilder.MapRoute("default", "");
+			routeBuilder.MapGet("time", context => {
+				return context.Response.WriteAsync(
+					DateTime.Now.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:sszzz"));
+			});
+			var routes = routeBuilder.Build();
+			app.UseRouter(routes);
+		}
+	}
 }
